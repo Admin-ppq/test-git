d1c02a234791a25dc2b3d6146377e0bed396bfe0
tom <tom@direct.com>
2022-06-15 14:42:41 +0800

    sencode commit

--- src/main/java/com/direct/git/TestGit.java
+++ src/main/java/com/direct/git/TestGit.java
@@ -3,5 +3,6 @@ package com.direct.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test---------------------");
+        System.out.println("test2---------------------");
     }
 }
