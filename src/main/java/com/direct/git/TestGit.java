e905533c464836db6b99b617fb5418a80074da10
tom <tom@direct.com>
2022-06-15 14:44:07 +0800

    first commit by hot_fix

--- src/main/java/com/direct/git/TestGit.java
+++ src/main/java/com/direct/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("test---------------------");
         System.out.println("test2---------------------");
+        System.out.println("test3 by hot_fix commit -----------");
     }
 }
