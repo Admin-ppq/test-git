212564e5bc3c6636206a8528dfcefaf404bcd475
Admin-ppq <107467211+Admin-ppq@users.noreply.github.com>
2022-06-15 16:10:42 +0800

    Update TestGit.java

--- src/main/java/com/direct/git/TestGit.java
+++ src/main/java/com/direct/git/TestGit.java
@@ -5,5 +5,7 @@ public class TestGit {
         System.out.println("test---------------------");
         System.out.println("test2---------------------");
         System.out.println("test3 by hot_fix commit -----------");
+        System.out.println("test34 by push --------执行---");
+        
     }
 }
